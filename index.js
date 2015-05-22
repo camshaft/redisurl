517baea2801511bedf5699accb133b24e33c4f5d
camshaft <bytheway.cameron@gmail.com>
2015-05-22 10:23:25 -0600

    style change

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 /**
  * Module dependencies
  */
-var redis = require("redis")
-  , url = require("url")
-  , d = require('domain').create();
+
+var redis = require('redis');
+var url = require('url');
+var d = require('domain').create();
 
 // Gracefully fail
-d.on("error", function() {});
+d.on('error', function() {});
 
 /**
  * Create a redis client from a url
@@ -14,22 +15,23 @@ d.on("error", function() {});
  * @param {String} redisUrl
  * @api public
  */
+
 module.exports = function(redisUrl) {
-  var options = url.parse(redisUrl || process.env.REDIS_URL || "redis://localhost:6379")
-    , client;
+  var options = url.parse(redisUrl || process.env.REDIS_URL || 'redis://localhost:6379');
+  var client;
 
   d.run(function() {
     client = redis.createClient(options.port, options.hostname);
 
     // Authorize the connection
-    if (options.auth) client.auth(options.auth.split(":")[1]);
+    if (options.auth) client.auth(options.auth.split(':')[1]);
 
     // Exit gracefully
     function close() {
       client.end();
     };
-    process.once("SIGTERM", close);
-    process.once("SIGINT", close);
+    process.once('SIGTERM', close);
+    process.once('SIGINT', close);
   })
 
   return client;
